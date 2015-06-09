f789b4ce813339dd5d7583503d20a636fe0521b9
Chris Cartlidge <cjcartlidge@yahoo.com>
2015-06-09 23:25:27 +0100

    Added missing files.

---
+++ Furious.Core.Query/IQuery.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IQuery.cs" company="Furious Solutions">
+//   Copyright © Furious Solutions 2015
+// </copyright>
+// <summary>
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Furious.Core.Query
+{
+    public interface IQuery<TProjection>
+    {
+    }
+}
+
---
+++ Furious.Core.Query/IQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IQueryHandler.cs" company="Furious Solutions">
+//   Copyright © Furious Solutions 2015
+// </copyright>
+// <summary>
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Furious.Core.Query
+{
+    public interface IQueryHandler<TQuery, TQueryProjection> where TQuery : IQuery<TQueryProjection>
+    {
+        TQueryProjection Handle(TQuery query);
+    }
+}
+
